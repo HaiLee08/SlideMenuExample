722714e8ade44f7a0ff981f0fbec983e50f639f9
Hai.Lee <vanhai89@gmail.com>
2013-08-02 10:21:46 +0700

    Initial Commit

---
+++ SlideMenuExample/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  SlideMenuExample
+//
+//  Created by HaiLee on 8/2/13.
+//  Copyright (c) 2013 HaiLee. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ SlideMenuExample/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  SlideMenuExample
+//
+//  Created by HaiLee on 8/2/13.
+//  Copyright (c) 2013 HaiLee. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
